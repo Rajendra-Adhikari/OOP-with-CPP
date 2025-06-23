68d8fe9729f5654cb68f46bc12c8390a46e16144
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-06-23 23:14:24 +0545

    rajendra-cpp

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Hello world!!!This is my first cpp program"<<endl;
+    cout<<"enter any two integer:";
+    cin>>x>>y;
+    cout<<"the sum is:  "<<x+y;
+    return 0;
+}
---
+++ overloading-func.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ void display(int l ,int b){
+    cout<<"area of  rectangle is"<<endl<<l*b;
+ }
+ void display(int l){
+    cout<<endl<<"area of square is"<<endl<<l*l;
+ }
+ int main(){
+    int a ,b;
+    cout<<"enter value of a and b (same if it is square)";
+    cin>>a>>b;
+    display(a,b);
+    display(a);
+    return 0;
+ }
+
